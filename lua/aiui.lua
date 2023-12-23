75d23ffe32a6f6b181f980f82a6c83bb37e43be7
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-12-23 19:38:42 +0100

    add chat to aiui

--- lua/aiui.lua
+++ lua/aiui.lua
@@ -1,4 +1,4 @@
-local chat_window = require("aiui.chat_window")
+local chat = require("aiui.Chat")
 local openai_curl = require("models.api.openai.openai_curl")
 local ollama_curl = require("models.api.ollama.ollama_curl")
 local chat_picker = require("aiui.chat_picker")
@@ -8,7 +8,7 @@ local modelMapper = require("aiui.modelMapper")
 return {
 	openai_curl = openai_curl,
 	ollama_curl = ollama_curl,
-	chat_window = chat_window,
+	chat = chat,
 	chat_picker = chat_picker,
 	utils = util,
 	model_mapper = modelMapper,
