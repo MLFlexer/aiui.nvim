1f31e9c85993150b185b37bc8e356f479cd00c14
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2023-12-23 21:20:24 +0100

    updated context_handler return type

--- lua/models/clients/ModelClient.lua
+++ lua/models/clients/ModelClient.lua
@@ -8,5 +8,5 @@
 ---@field name string
 ---@field private command string
 ---@field private args string[]
----@field context_handler fun(new_context: any, old_context: any)
+---@field context_handler fun(new_context: any, old_context: any): any
 ---@field request fun(self: ModelClient, model_name: string, request_msg: string[], system_msg: string, context: any, result_handler: result_handler, error_handler: error_handler, context_handler: context_handler)
