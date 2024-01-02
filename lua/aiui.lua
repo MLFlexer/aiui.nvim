a918cedf6f1337bb2572c944f49456946eb4918b
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-01-02 16:14:20 +0100

    removed old files

--- lua/aiui.lua
+++ lua/aiui.lua
@@ -1,15 +1,6 @@
-local chat = require("aiui.Chat")
-local openai_curl = require("models.clients.openai.openai_curl")
-local ollama_curl = require("models.clients.ollama.ollama_curl")
-local chat_picker = require("aiui.chat_picker")
-local util = require("aiui.util")
-local modelMapper = require("aiui.modelMapper")
+local Chat = require("aiui.Chat")
+local ModelCollection = require("aiui.ModelCollection")
 
 return {
-	openai_curl = openai_curl,
-	ollama_curl = ollama_curl,
-	chat = chat,
-	chat_picker = chat_picker,
-	utils = util,
-	model_mapper = modelMapper,
+	chat = Chat,
 }
