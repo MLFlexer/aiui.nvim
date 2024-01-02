1074ac37cea29be853eb99855c59953a838c127d
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-01-02 20:50:47 +0100

    add get visual text and lines

---
+++ lua/aiui/diff.lua
@@ -0,0 +1,47 @@
+local diff = {}
+
+---returns a table of lines, position and buffer number
+---@return {lines: string[], start_row: integer, start_col: integer, end_col: integer, end_row: integer, bufnr: integer}
+function diff.get_visual_text()
+	local start_pos = vim.fn.getpos("'<")
+	local end_pos = vim.fn.getpos("'>")
+	local result = {
+		start_row = start_pos[2],
+		start_col = start_pos[3],
+		end_row = end_pos[2],
+		end_col = end_pos[3],
+		bufnr = start_pos[1],
+	}
+
+	if result.end_col == 2147483647 then
+		result.lines = vim.api.nvim_buf_get_lines(result.bufnr, result.start_row, result.end_row, false)
+	else
+		result.lines = vim.api.nvim_buf_get_text(
+			result.bufnr,
+			result.start_row,
+			result.start_col,
+			result.end_row,
+			result.end_col,
+			{}
+		)
+	end
+
+	return result
+end
+
+---returns a table of lines, position and buffer number
+---@return {lines: string[], start_row: integer, end_row: integer, bufnr: integer}
+function diff.get_visual_lines()
+	local start_pos = vim.fn.getpos("'<")
+	local end_pos = vim.fn.getpos("'>")
+	local result = {
+		start_row = start_pos[2],
+		end_row = end_pos[2],
+		bufnr = start_pos[1],
+	}
+
+	result.lines = vim.api.nvim_buf_get_lines(result.bufnr, result.start_row, result.end_row, false)
+	return result
+end
+
+return diff
