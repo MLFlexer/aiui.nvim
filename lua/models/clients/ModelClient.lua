fc57fec153d90567beeb8623cdd5607157725361
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-01-09 17:24:07 +0100

    add default models to openai client

--- lua/models/clients/ModelClient.lua
+++ lua/models/clients/ModelClient.lua
@@ -5,8 +5,6 @@
 
 ---@class ModelClient
 ---@field name string
----@field private command string
----@field private args string[]
 ---@field context_handler fun(new_context: any, old_context: any): any
 ---@field request fun(self: ModelClient, model_name: string, request_msg: string[], system_msg: string, context: any, result_handler: result_handler, error_handler: error_handler, context_handler: context_handler)
 ---@field stream_request fun(self: ModelClient, model_name: string, request_msg: string[], system_msg: string, context: any, chunk_handler: chunk_handler, context_handler: context_handler)
