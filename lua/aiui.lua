67ef1e598b6f2f4731ca9009a42f60540d463757
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2024-01-28 14:06:04 +0100

    add test functions

--- lua/aiui.lua
+++ lua/aiui.lua
@@ -1,3 +1,15 @@
+function P(v)
+	print(vim.inspect(v))
+	return v
+end
+
+vim.api.nvim_create_user_command("AA", function()
+	package.loaded["aiui.context"] = nil
+	local context = require("aiui.context")
+	context.get_cwd_files()
+	context.get_buffers()
+end, {})
+
 return {
 	add_defaults = function()
 		require("defaults").initialize()
